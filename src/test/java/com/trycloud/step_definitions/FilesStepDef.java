d7e2575f44729310e4773ec1fb4c6e0609cd5028
cagri <cataseven3443@gmail.com>
2020-02-10 21:12:49 +0100

    new version from cagri

--- src/test/java/com/trycloud/step_definitions/FilesStepDef.java
+++ src/test/java/com/trycloud/step_definitions/FilesStepDef.java
@@ -4,11 +4,15 @@ import com.trycloud.pages.FilesPage;
 import com.trycloud.utilities.ConfigurationReader;
 import io.cucumber.java.en.Then;
 
+import java.util.Random;
+
 public class FilesStepDef {
     @Then("the user add new files")
     public void the_user_add_new_files() {
         FilesPage file = new FilesPage();
         String filename = ConfigurationReader.get("fileName");
-        file.plus(filename);
+        Random rn=new Random();
+        int x=rn.nextInt(50);
+        file.plus(filename+x);
     }
 }
